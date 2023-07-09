12d3ba6c33d1bf10e0979cb29e93742832da236a
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2023-07-08 21:05:07 -0500

    edit: check that files exist and -o flag

--- generate-video.py
+++ generate-video.py
@@ -1,16 +1,28 @@
+# Examples
+#
+# $ python3 generate-video.py -p 43-1 -i ./timestamps/43-1.vtt -ss 00:00:10 -to 00:00:20 -o output.mp4
+
 import subprocess
 import argparse
 import itertools
+import os
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument(
     "-p",
-    dest = "prefix")
+    dest = "prefix",
+    required = True)
+
+parser.add_argument(
+    "-i",
+    dest = "filename_input",
+    required = True)
 
 parser.add_argument(
     "-o",
-    dest = "filename_output")
+    dest = "filename_output",
+    required = True)
 
 parser.add_argument(
     "-ss",
@@ -23,11 +35,20 @@ parser.add_argument(
 args = parser.parse_args()
 
 filename_prefix = args.prefix
-filename_video = f'{filename_prefix}-timestamps.webm'
-filename_subtitles_pinyin = f'{filename_prefix}.pinyin.vtt'
-filename_subtitles_zh_hans = f'{filename_prefix}.zh-hans.vtt'
-filename_subtitles_en = f'{filename_prefix}.en.vtt'
-filename_output = args.filename_output if args.filename_output else None
+filename_subtitles_pinyin = f'subtitles/{filename_prefix}.pinyin.vtt'
+filename_subtitles_zh_hans = f'subtitles/{filename_prefix}.zh-hans.vtt'
+filename_subtitles_en = f'subtitles/{filename_prefix}.en.vtt'
+
+files_that_should_exist = [
+    filename_subtitles_pinyin,
+    filename_subtitles_zh_hans,
+    filename_subtitles_en,
+    args.filename_input,
+]
+
+for f in files_that_should_exist:
+    if not os.path.isfile(f):
+        raise Exception(f"The following file doesn't exist: {f}.")
 
 subtitles=(f"subtitles={filename_subtitles_pinyin}:force_style='fontname=Noto Sans,fontsize=20,MarginV=56,Outline=0',"
            + f"subtitles={filename_subtitles_zh_hans}:force_style='fontname=Noto Sans Mono CJK SC Regular,fontsize=32,MarginV=25,Outline=0,PrimaryColour=&H0000FFFF,Bold=1,Spacing=3',"
@@ -38,9 +59,9 @@ cmd = list(itertools.chain.from_iterable(
                   '-y',
                   '-f', 'lavfi',
                   '-i', 'color=c=black:s=1920x1080',
-                  '-i', filename_video],
+                  '-i', args.filename_input],
                  ['-ss', args.start_time, '-to', args.end_time] if args.start_time and args.end_time else None,
                  ['-filter_complex', f'[1:v]scale=-1:1080 [ovrl],[0:v][ovrl]overlay=(main_w-overlay_w)/2:0:shortest=1,drawbox=y=ih-300:height=300:t=fill:color=black@0.7,{subtitles}',
-                  filename_output]] if x is not None]))
+                  args.filename_output]] if x is not None]))
 
 subprocess.run(cmd)
