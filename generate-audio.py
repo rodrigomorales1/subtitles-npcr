20e7a6b9de03491e643e76cfed7ae005c50fc713
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2023-07-21 16:26:33 -0500

    edit: create a subtitle for each line

--- generate-audio.py
+++ generate-audio.py
@@ -86,81 +86,82 @@ def generate_ass_file(data_timestamps_sentences, field_for_subtitles_in_third_li
     temporary_file_1 = tempfile.NamedTemporaryFile(suffix='.ass')
     temporary_file_2 = tempfile.NamedTemporaryFile(suffix='.ass')
     temporary_file_3 = tempfile.NamedTemporaryFile(suffix='.ass')
+    temporary_file_4 = tempfile.NamedTemporaryFile(suffix='.ass')
+    temporary_file_5 = tempfile.NamedTemporaryFile(suffix='.ass')
+    temporary_file_6 = tempfile.NamedTemporaryFile(suffix='.ass')
+    temporary_file_7 = tempfile.NamedTemporaryFile(suffix='.ass')
+    temporary_file_8 = tempfile.NamedTemporaryFile(suffix='.ass')
+    temporary_file_9 = tempfile.NamedTemporaryFile(suffix='.ass')
 
     with (open(temporary_file_1.name, 'w') as t1,
           open(temporary_file_2.name, 'w') as t2,
-          open(temporary_file_3.name, 'w') as t3):
+          open(temporary_file_3.name, 'w') as t3,
+          open(temporary_file_4.name, 'w') as t4,
+          open(temporary_file_5.name, 'w') as t5,
+          open(temporary_file_6.name, 'w') as t6,
+          open(temporary_file_7.name, 'w') as t7,
+          open(temporary_file_8.name, 'w') as t8,
+          open(temporary_file_9.name, 'w') as t9):
 
-        t1.write("""[Script Info]
+        for t in [t1, t2, t3, t4, t5, t6, t7, t8, t9]:
+            t.write("""[Script Info]
 ScriptType: v4.00+
 
 [V4+ Styles]
-Format: Name,               Fontname,                      Fontsize, Outline,     PrimaryColour, Shadow, Bold, Alignment, MarginV
-Style:  group-1-pinyin,     Noto Sans,                     18,       0,           &HFFFFFF,      0,      0,    2,         258
-Style:  group-1-zh-hans,    Noto Sans Mono CJK SC Regular, 32,       0,           &HFFFFFF,      0,      0,    2,         223
-Style:  group-1-third-line, Noto Sans,                     18,       0,           &HFFFFFF,      0,      0,    2,         203
+Format: Name, Fontname, Fontsize, Outline, PrimaryColour, Shadow, Bold, Alignment, MarginV""")
 
-[Events]
-Format: Start, End, Style, Text""")
+        t1.write("\nStyle: group-1-pinyin, Noto Sans, 18, 0, &HFFFFFF, 0, 0, 2, 258")
+        t2.write("\nStyle: group-1-zh-hans, Noto Sans Mono CJK SC Regular, 32, 0, &HFFFFFF, 0, 0, 2, 223")
+        t3.write("\nStyle: group-1-third-line, Noto Sans, 18, 0, &HFFFFFF, 0, 0, 2, 203")
 
-        t2.write("""[Script Info]
-ScriptType: v4.00+
+        t4.write("\nStyle: group-2-pinyin, Noto Sans, 18, 0, &HFFFFFF, 0, 0, 2, 164")
+        t5.write("\nStyle: group-2-zh-hans, Noto Sans Mono CJK SC Regular, 32, 0, &HFFFFFF, 0, 0, 2, 129")
+        t6.write("\nStyle: group-2-third-line, Noto Sans, 18, 0, &HFFFFFF, 0, 0, 2, 109")
 
-[V4+ Styles]
-Format: Name,               Fontname,                      Fontsize, Outline,     PrimaryColour, Shadow, Bold, Alignment, MarginV
-Style:  group-2-pinyin,     Noto Sans,                     18,       0,           &HFFFFFF,      0,      0,    2,         154
-Style:  group-2-zh-hans,    Noto Sans Mono CJK SC Regular, 32,       0,           &HFFFFFF,      0,      0,    2,         129
-Style:  group-2-third-line, Noto Sans,                     18,       0,           &HFFFFFF,      0,      0,    2,         109
+        t7.write("\nStyle: group-3-pinyin, Noto Sans, 18, 0, &HFFFFFF, 0, 0, 2, 67")
+        t8.write("\nStyle: group-3-zh-hans, Noto Sans Mono CJK SC Regular, 32, 0, &HFFFFFF, 0, 0, 2, 32")
+        t9.write("\nStyle: group-3-third-line, Noto Sans, 18, 0, &HFFFFFF, 0, 0, 2, 12")
 
-[Events]
-Format: Start, End, Style, Text""")
-
-        t3.write("""[Script Info]
-ScriptType: v4.00+
-
-[V4+ Styles]
-Format: Name,               Fontname,                      Fontsize, Outline,     PrimaryColour, Shadow, Bold, Alignment, MarginV
-Style:  group-3-pinyin,     Noto Sans,                     18,       0,           &HFFFFFF,      0,      0,    2,         57
-Style:  group-3-zh-hans,    Noto Sans Mono CJK SC Regular, 32,       0,           &HFFFFFF,      0,      0,    2,         32
-Style:  group-3-third-line, Noto Sans,                     18,       0,           &HFFFFFF,      0,      0,    2,         12
-
-[Events]
+        for t in [t1, t2, t3, t4, t5, t6, t7, t8, t9]:
+            t.write("""\n[Events]
 Format: Start, End, Style, Text""")
 
         for i in range(0, len(data_timestamps_sentences), 3):
             start_time = data_timestamps_sentences[i]['start']
             if i == len(data_timestamps_sentences) - 1:
                 end_time = data_timestamps_sentences[i]['end']
-                t1.write(f"""
-Dialogue: {start_time}, {end_time}, group-1-zh-hans, {data_timestamps_sentences[i]['zh-hans']}
-Dialogue: {start_time}, {end_time}, group-1-pinyin, {data_timestamps_sentences[i]['pinyin']}
-Dialogue: {start_time}, {end_time}, group-1-third-line, {data_timestamps_sentences[i][field_for_subtitles_in_third_line]}""")
+                t1.write(f"\nDialogue: {start_time}, {end_time}, group-1-pinyin, {data_timestamps_sentences[i]['pinyin']}")
+                t2.write(f"\nDialogue: {start_time}, {end_time}, group-1-zh-hans, {data_timestamps_sentences[i]['zh-hans']}")
+                t3.write(f"\nDialogue: {start_time}, {end_time}, group-1-third-line, {data_timestamps_sentences[i][field_for_subtitles_in_third_line]}")
             elif i + 1 == len(data_timestamps_sentences) - 1:
                 end_time = data_timestamps_sentences[i+1]['end']
-                t1.write(f"""
-Dialogue: {start_time}, {end_time}, group-1-zh-hans, {data_timestamps_sentences[i]['zh-hans']}
-Dialogue: {start_time}, {end_time}, group-1-pinyin, {data_timestamps_sentences[i]['pinyin']}
-Dialogue: {start_time}, {end_time}, group-1-third-line, {data_timestamps_sentences[i][field_for_subtitles_in_third_line]}""")
-                t2.write(f"""
-Dialogue: {start_time}, {end_time}, group-2-zh-hans, {data_timestamps_sentences[i+1]['zh-hans']}
-Dialogue: {start_time}, {end_time}, group-2-pinyin, {data_timestamps_sentences[i+1]['pinyin']}
-Dialogue: {start_time}, {end_time}, group-2-third-line, {data_timestamps_sentences[i+1][field_for_subtitles_in_third_line]}""")
+                t1.write(f"\nDialogue: {start_time}, {end_time}, group-1-pinyin, {data_timestamps_sentences[i]['pinyin']}")
+                t2.write(f"\nDialogue: {start_time}, {end_time}, group-1-zh-hans, {data_timestamps_sentences[i]['zh-hans']}")
+                t3.write(f"\nDialogue: {start_time}, {end_time}, group-1-third-line, {data_timestamps_sentences[i][field_for_subtitles_in_third_line]}")
+                t4.write(f"\nDialogue: {start_time}, {end_time}, group-2-pinyin, {data_timestamps_sentences[i+1]['pinyin']}")
+                t5.write(f"\nDialogue: {start_time}, {end_time}, group-2-zh-hans, {data_timestamps_sentences[i+1]['zh-hans']}")
+                t6.write(f"\nDialogue: {start_time}, {end_time}, group-2-third-line, {data_timestamps_sentences[i+1][field_for_subtitles_in_third_line]}")
             else:
                 end_time = data_timestamps_sentences[i+2]['end']
-                t1.write(f"""
-Dialogue: {start_time}, {end_time}, group-1-zh-hans, {data_timestamps_sentences[i]['zh-hans']}
-Dialogue: {start_time}, {end_time}, group-1-pinyin, {data_timestamps_sentences[i]['pinyin']}
-Dialogue: {start_time}, {end_time}, group-1-third-line, {data_timestamps_sentences[i][field_for_subtitles_in_third_line]}""")
-                t2.write(f"""
-Dialogue: {start_time}, {end_time}, group-2-zh-hans, {data_timestamps_sentences[i+1]['zh-hans']}
-Dialogue: {start_time}, {end_time}, group-2-pinyin, {data_timestamps_sentences[i+1]['pinyin']}
-Dialogue: {start_time}, {end_time}, group-2-third-line, {data_timestamps_sentences[i+1][field_for_subtitles_in_third_line]}""")
-                t3.write(f"""
-Dialogue: {start_time}, {end_time}, group-3-zh-hans, {data_timestamps_sentences[i+2]['zh-hans']}
-Dialogue: {start_time}, {end_time}, group-3-pinyin, {data_timestamps_sentences[i+2]['pinyin']}
-Dialogue: {start_time}, {end_time}, group-3-third-line, {data_timestamps_sentences[i+2][field_for_subtitles_in_third_line]}""")
+                t1.write(f"\nDialogue: {start_time}, {end_time}, group-1-pinyin, {data_timestamps_sentences[i]['pinyin']}")
+                t2.write(f"\nDialogue: {start_time}, {end_time}, group-1-zh-hans, {data_timestamps_sentences[i]['zh-hans']}")
+                t3.write(f"\nDialogue: {start_time}, {end_time}, group-1-third-line, {data_timestamps_sentences[i][field_for_subtitles_in_third_line]}")
+                t4.write(f"\nDialogue: {start_time}, {end_time}, group-2-pinyin, {data_timestamps_sentences[i+1]['pinyin']}")
+                t5.write(f"\nDialogue: {start_time}, {end_time}, group-2-zh-hans, {data_timestamps_sentences[i+1]['zh-hans']}")
+                t6.write(f"\nDialogue: {start_time}, {end_time}, group-2-third-line, {data_timestamps_sentences[i+1][field_for_subtitles_in_third_line]}")
+                t7.write(f"\nDialogue: {start_time}, {end_time}, group-3-pinyin, {data_timestamps_sentences[i+2]['pinyin']}")
+                t8.write(f"\nDialogue: {start_time}, {end_time}, group-3-zh-hans, {data_timestamps_sentences[i+2]['zh-hans']}")
+                t9.write(f"\nDialogue: {start_time}, {end_time}, group-3-third-line, {data_timestamps_sentences[i+2][field_for_subtitles_in_third_line]}")
 
-    return [temporary_file_1, temporary_file_2, temporary_file_3]
+    return [temporary_file_1,
+            temporary_file_2,
+            temporary_file_3,
+            temporary_file_4,
+            temporary_file_5,
+            temporary_file_6,
+            temporary_file_7,
+            temporary_file_8,
+            temporary_file_9]
 
 def generate_rectangle(data_timestamps_sentences, name_last_overlay):
     result =""
@@ -224,8 +225,8 @@ def generate_video(media, output, start_time, end_time, field_for_subtitles_in_t
     # notation might be inserted in values in some keys in the
     # dictionary that contains all the data.
     metadata_file = generate_metadata_file(data_timestamps_sentences)
-
     ass_files = generate_ass_file(data_timestamps_sentences, field_for_subtitles_in_third_line)
+    subtitles = ','.join([f'subtitles={x.name}' for x in ass_files])
 
     height = int(height)
     width = int(16 * (height/9))
@@ -245,7 +246,7 @@ def generate_video(media, output, start_time, end_time, field_for_subtitles_in_t
                                  + ",[0:v][my_group_separator_1] overlay=0:358:shortest=1 [my_overlay_1]"
                                  + ",[my_overlay_1][my_group_separator_2] overlay=0:719:shortest=1"
                                  + ('[foo]' + generate_rectangle(data_timestamps_sentences, 'foo') if highlight_background_of_sentence_being_read else "")
-                                 + f",subtitles={ass_files[0].name},subtitles={ass_files[1].name},subtitles={ass_files[2].name}"),
+                                 + ',' + subtitles),
              '-c:a', 'copy',
              '-shortest',
              output]
