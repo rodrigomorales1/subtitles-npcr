dfa0b60bc3aa4375505b8b11ceb6b659ec8b0bc1
Rodrigo Morales <moralesrodrigo1100@gmail.com>
2023-08-31 17:41:06 -0500

    fix: replace all timestamps with two decimals

--- python/subtitles_single_group_below.py
+++ python/subtitles_single_group_below.py
@@ -80,6 +80,7 @@ def generate_video(file_path_media,
     keys_for_bottom_subtitles = keys_for_bottom_subtitles.split(',')
 
     data_timestamps_sentences = utilities.get_data_timestamps_sentences(file_path_timestamps, file_path_sentences)
+    utilities.ensure_timestamps_have_two_decimals(data_timestamps_sentences)
 
     ass_file = generate_ass_file(data_timestamps_sentences, do_color_words, keys_for_bottom_subtitles)
 
